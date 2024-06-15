d9093bfae2fb230b19b6e082e0f10136d9385a53
Dipakkandel-2004 <137183953+Dipakkandel-2004@users.noreply.github.com>
2024-06-15 09:26:37 +0545

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,27 @@
+f=open("studentdetail.txt","a+")
+class student:
+    def __init__(self,name,address,DOB,Father_name):
+        self.name=name
+        self.address=address
+        self.DOB=DOB
+        self.Father_name=Father_name
+    def printname(self):
+        print(self.name)   
+        print(self.address)
+        print(self.DOB)
+        print(self.Father_name)        
+j=int(input("Enter the number of student :"))
+i=0
+a=""
+while(i<j):
+    a="s"+str(i)
+    name=input("Enter Name")
+    add=input("Enter Address")
+    dob=input("Enter DOB")
+    father=input("Enter Father name")    
+    a=student(name,add,dob,father)
+    b=("Name="+a.name+"\t"+"Address="+a.address+"\t"+"DOB="+a.DOB+"\t"+"Father name="+a.Father_name+"\n")
+    f.write(b)
+    print("\n")
+    i+=1
+f.close()
